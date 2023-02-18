9e1c8511045544b1f5eed6361ddf6d5ede79feb5
Philemon E <97684779+mcenny01@users.noreply.github.com>
2023-02-18 17:25:43 +0100

    Update dashboard-layout.tsx
    modified comment

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -7,7 +7,8 @@ type DashboardLayoutProps = {
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
     return (
         <div style={{ width: '100vw', display: 'flex' }}>
-            <div style={{ width: '20vw', height: '100Vh', border: '1px solid red', position: 'sticky', top: '0' }}>SideBar</div> // this is sort layout template component
+            // this is sort layout template component
+            <div style={{ width: '20vw', height: '100Vh', border: '1px solid red', position: 'sticky', top: '0' }}>SideBar</div> 
             {children}
         </div>
     );
